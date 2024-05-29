495fac81c42a68710144a3d64bd338c47575ed8e
SheikJack <kealebogakido@gmail.comm>
2024-05-29 16:13:42 +0200

    first commit

---
+++ src/dummydata.js
@@ -0,0 +1,414 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "SDA (Science Double Award)",
+    desc: "The exam aims to evaluate students' knowledge and proficiency in various scientific principles, theories, and practical applications.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Mathematics",
+    desc: "Students are tested on their ability to solve mathematical problems, apply mathematical principles to real-world situations.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "English",
+    desc: "evaluates students' reading, writing, listening, and speaking skills, with the goal of ensuring that students have a strong foundation in English communication for further education and practical use..",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "300",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "3",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "10",
+    title: "FLEXIBLE SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "5",
+    title: "SUBJECTS",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "SDA (Science Double Award)",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by John Smith",
+        totalTime: "5 lectures/week (190 hrs)",
+      },
+    ],
+    priceAll: "P5,400 All Year",
+    pricePer: "P600 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Integrated Science",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "P2,400 All Year",
+    pricePer: "P300 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "Mathematics (JSE$BGCSE)",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "P2,400 All Year",
+    pricePer: "P300 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Secondary Mathematics",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Thabo Masilo",
+        totalTime: "12 lectures (2 hrs/w)",
+      },
+    ],
+    priceAll: "P2,400 All Year",
+    pricePer: "P300 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Junior Level Mathematics",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "Kabo Moremi",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "P2,400 All Year",
+    pricePer: "P300 per month",
+  },
+  // {
+  //   id: 6,
+  //   cover: "../images/courses/c6.png",
+  //   coursesName: "Learn Frontend Programming Language",
+  //   courTeacher: [
+  //     {
+  //       dcover: "./images/back.webp",
+  //       name: "by Sunil Bishowkaram",
+  //       totalTime: "200 lectures (300 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$500 All Course",
+  //   pricePer: "$80 per month",
+  // },
+  // {
+  //   id: 7,
+  //   cover: "../images/courses/c7.png",
+  //   coursesName: "Introducing to with HTML / CSS",
+  //   courTeacher: [
+  //     {
+  //       dcover: "./images/back.webp",
+  //       name: "by Price",
+  //       totalTime: "20 lectures (50 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$20 All Course",
+  //   pricePer: "$2 per month",
+  // },
+  // {
+  //   id: 8,
+  //   cover: "../images/courses/c8.png",
+  //   coursesName: "Introducing to with JAVA",
+  //   courTeacher: [
+  //     {
+  //       dcover: "./images/back.webp",
+  //       name: "by Petter",
+  //       totalTime: "80 lectures (200 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$200 All Course",
+  //   pricePer: "$40 per month",
+  // },
+  // {
+  //   id: 9,
+  //   cover: "../images/courses/c9.png",
+  //   coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+  //   courTeacher: [
+  //     {
+  //       dcover: "./images/back.webp",
+  //       name: "by Petter",
+  //       totalTime: "50 lectures (50 hrs)",
+  //     },
+  //   ],
+  //   priceAll: "$20 All Course",
+  //   pricePer: "$3 per month",
+  // },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "Mathematics",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Intergrated Sciences",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Science Double Award",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "Geography",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "English",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Pure Sciences",
+    course: "60 Courses",
+  },
+  // {
+  //   cover: "./images/courses/online/o7.png",
+  //   hoverCover: "./images/courses/online/o7.1.png",
+  //   courseName: "Health & Fitness",
+  //   course: "10 Courses",
+  // },
+  // {
+  //   cover: "./images/courses/online/o8.png",
+  //   hoverCover: "./images/courses/online/o8.1.png",
+  //   courseName: "Marketing",
+  //   course: "30 Courses",
+  // },
+  // {
+  //   cover: "./images/courses/online/o9.png",
+  //   hoverCover: "./images/courses/online/o9.1.png",
+  //   courseName: "Graphic Design",
+  //   course: "80 Courses",
+  // },
+  // {
+  //   cover: "./images/courses/online/o10.png",
+  //   hoverCover: "./images/courses/online/o10.1.png",
+  //   courseName: "Music",
+  //   course: "120 Courses",
+  // },
+  // {
+  //   cover: "./images/courses/online/o11.png",
+  //   hoverCover: "./images/courses/online/o11.1.png",
+  //   courseName: "Business Administration",
+  //   course: "17 Courses",
+  // },
+  // {
+  //   cover: "./images/courses/online/o12.png",
+  //   hoverCover: "./images/courses/online/o12.1.png",
+  //   courseName: "Web Management",
+  //   course: "17 Courses",
+  // },
+]
+export const team = [
+  {
+    cover: "./images/team/mogo.jpg",
+    name: "Mogomotsi Masalele",
+    work: "STUDENT AND INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/tsholo.jpg",
+    name: "Tsholofelo Moreti",
+    work: "DEVELOPER AND INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/lerato.jpeg",
+    name: "Lerato",
+    work: "GRADUATE AND LEAD INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/kabo.jpg",
+    name: "Kabo Kakgolo",
+    work: "STUDENT AND LEAD INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/reba.jpg",
+    name: "Rebabedi Kopano",
+    work: "STUDENT AND LEAD INSTRUCTOR",
+  },
+  // {
+  //   cover: "./images/team/t6.webp",
+  //   name: "Ph.D Arthur MaGregor",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+  // {
+  //   cover: "./images/team/t7.webp",
+  //   name: "Ph.D Anna Hanzen",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+  // {
+  //   cover: "./images/team/t8.webp",
+  //   name: "Ph.D Brian Wooden",
+  //   work: "DEVELOPER AND LEAD INSTRUCTOR",
+  // },
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "300",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "450",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "600",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "1200",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+]
+export const faq = [
+  {
+    title: "How to Enroll online to these Subject?",
+    desc: "For now, online registration is not available but you will have to use the contact section or a whatsapp button to send a text that will help you get registered",
+  },
+  {
+    title: "When is our intake",
+    desc: "Our intake is ussually at the beginning of the year, it may continue untill all spaces a filled. ",
+  },
+  {
+    title: "How can your tuition service help our kids?",
+    desc: "Our tuition offers a supportive and tailored learning environment that can enhance a child's educational journey, leading to improved understanding, confidence, and academic success.",
+  },
+  {
+    title: "What is your pass rate?",
+    desc: "Our excellent pass rate is often a key indicator of the effectiveness of an educational program or institution, demonstrating our ability to prepare students well for the assessment and ensuring that a significant portion of students achieve the desired outcomes. With that said, we almost always stand at a range of 89-90%",
+  },
+  {
+    title: "How many kids have you helped get to University",
+    desc: "Our outstanding track record extends beyond merely ensuring students pass their examinations. We consistently empower students to not only succeed academically but also gain admission to their desired university courses, marking our commitment to comprehensive educational success.",
+  },
+  // {
+  //   title: "How It is hidden by default, until the collapse?",
+  //   desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  // },
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "3 COMMENTS ",
+    title: "Build your Dream  Civil Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "5 COMMENTS ",
+    title: "How to become a Registered Nurse",
+    desc: "If you are not brave enough no one will back you up.",
+    cover: "../images/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "People cry, not because they are weak, because they're been strong too long.",
+    cover: "../images/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "JAN. 02, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream in Clinical pshycology",
+    desc: "There is no chance, no destiny, no fate that can hinder the firm resolve of a determined soul.",
+    cover: "../images/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "DEC. 14, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "You can take a horse to the river, you cant force it to drink.",
+    cover: "../images/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "JAN. 18, 2021",
+    com: "12 COMMENTS ",
+    title: "Build your Dream Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/blog/b6.webp",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "Thabo Moseki",
+    post: "University Student",
+    desc: "Re-took his sciences double award with mathematics and passed, currently studying Information Technology at Botho Unviversity.",
+    cover: "./images/testo/moseki.jpg",
+  },
+  {
+    id: 2,
+    name: "Rebecca Moreetse",
+    post: "University Student",
+    desc: "Re-took her sciences double award with mathematics and passed, currently trainning to be a Nurse.",
+    cover: "./images/testo/rebe.jpg",
+  },
+  {
+    id: 3,
+    name: "Veronicah Tshabang",
+    post: "Student",
+    desc: "She had basic problems in Mathematics before joining us in 2022, she passed with a distinction now in form 4 at Molefi Senior School.",
+    cover: "./images/testo/vero.png",
+  },
+]
